f19c75cc1971b052a69adcfd9e82f6587328a39a
ProcopVladimirAlexandru <procopvladimir01@gmail.com>
2017-04-17 22:24:35 +0300

    Add files via upload

---
+++ uite aici/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name funwebregandit1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the funwebregandit1App
+ */
+angular.module('funwebregandit1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
